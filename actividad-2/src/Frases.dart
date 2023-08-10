c21fd2a7d4a43e47995cc1e824a2322af54f7bd7
SantiagoYpes <santiyezu8@hotmail.com>
2023-08-10 17:02:34 -0500

    Organizar repo

--- actividad-2/src/Frases.dart
+++ actividad-2/src/Frases.dart
@@ -3,6 +3,7 @@ import '../models/frase.dart';
 import 'package:http/http.dart' as http;
 
 Future consultarFrases() async {
+  
   // This example uses the Google Books API to search for books about http.
   // https://developers.google.com/books/docs/overview
 
@@ -27,4 +28,5 @@ Future consultarFrases() async {
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
+  
 }
